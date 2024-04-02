7bebf21d3fd5613fd3b5367474ad3801ea7eee62
LDcourrielleur <93119484+LDcourrielleur@users.noreply.github.com>
2024-04-02 12:08:35 +0200

    0025-Modification-url-serveur-anais-url-de-production

--- defaults/pref/anais.js
+++ defaults/pref/anais.js
@@ -1,2 +1,2 @@
 //url d'acces au service anais
-pref("anais.anaismoz.urlserveur", "http://anais-test.si.minint.fr/anais/anaism2.php");
+pref("anais.anaismoz.urlserveur", "https://proxyldapanais.si.minint.fr/anais/anaism2.php");
